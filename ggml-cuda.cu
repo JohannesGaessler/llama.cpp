5bf64cca0a2d325122686cc4d7c87e68668f0373
JohannesGaessler <johannesg@5d6.de>
2023-08-07 23:47:54 +0200

    q4_K 618 t/s

--- ggml-cuda.cu
+++ ggml-cuda.cu
@@ -4425,8 +4425,8 @@ static void ggml_mul_mat_q4_K_q8_1_cuda(
         }
     } else {
         const int mmq_x  = 64;
-        const int mmq_y  = 128;
-        const int nwarps = 4;
+        const int mmq_y  = 64;
+        const int nwarps = 8;
 
         const int block_num_x = (nrows_x + mmq_y - 1) / mmq_y;
         const int block_num_y = (ncols_y + mmq_x - 1) / mmq_x;
