20c010a68b9dc257b749a5dd1f6c4c3d0b961786
JohannesGaessler <johannesg@5d6.de>
2023-08-12 18:19:41 +0200

    WIP better packing

--- ggml-cuda.cu
+++ ggml-cuda.cu
@@ -2820,24 +2820,20 @@ template <int mmq_y, int nwarps, bool need_check> static __device__ __forceinlin
         //  m03: 1, 0, 1, 0
         //  m47: 2, 1, 1, 1
 
-        int scales8 = 0;
+        int scales8 = (scales[2*(ksc/2)] >> (16 * (ksc==1) + 4 * (ksc == 3))) & 0x00000F0F;
         if (ksc == 0) {
-            scales8 |= (scales[0] >>  0) & 0x00000F0F;
             scales8 |= (scales[0] >>  0) & 0x00003030;
             scales8 |= (scales[1] << 16) & 0x0F0F0000;
             scales8 |= (scales[1] << 16) & 0x30300000;
         } else if (ksc == 1) {
-            scales8 |= (scales[0] >> 16) & 0x00000F0F;
             scales8 |= (scales[0] >> 16) & 0x00003030;
             scales8 |= (scales[1] <<  0) & 0x0F0F0000;
             scales8 |= (scales[1] <<  0) & 0x30300000;
         } else if (ksc == 2) {
-            scales8 |= (scales[2] >>  0) & 0x00000F0F;
             scales8 |= (scales[0] >>  2) & 0x00003030;
             scales8 |= (scales[2] << 12) & 0x0F0F0000;
             scales8 |= (scales[1] << 14) & 0x30300000;
         } else {
-            scales8 |= (scales[2] >>  4) & 0x00000F0F;
             scales8 |= (scales[0] >> 18) & 0x00003030;
             scales8 |= (scales[2] >>  4) & 0x0F0F0000;
             scales8 |= (scales[1] >>  2) & 0x30300000;
