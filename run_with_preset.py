487f6226108b3d31a283aeced1468fc72ccdeaea
JohannesGaessler <johannesg@5d6.de>
2023-08-23 19:58:35 +0200

    better run_with_preset.py CLI

--- run_with_preset.py
+++ run_with_preset.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
+import argparse
 import os
-import signal
 import subprocess
 import sys
 
@@ -21,8 +21,40 @@ CLI_ARGS_MAIN = [
     "temp", "tfs", "top-k", "top-p", "typical", "verbose-prompt"
 ]
 
-with open(sys.argv[1], "r") as f:
-    props = yaml.load(f, yaml.SafeLoader)
+description = """Run llama.cpp binaries with presets from YAML file(s).
+To specify which binary should be run, specify the "binary" property.
+To get a preset file template, run a llama.cpp binary with the "--logdir" CLI argument.
+
+Formatting considerations:
+- The YAML property names are the same as the CLI argument names of the corresponding binary.
+- Properties must use the long name of their corresponding llama.cpp CLI arguments.
+- Like the llama.cpp binaries the property names do not differentiate between hyphens and underscores.
+- Flags must be defined as "<PROPERTY_NAME>: true" to be effective.
+- To define the logit_bias property, the expected format is "<TOKEN_ID>: <BIAS>" in the "logit_bias" namespace.
+- To define multiple "reverse_prompt" properties simultaneously the expected format is a list of strings.
+- To define a tensor split, pass a list of floats.
+"""
+usage = "run_with_preset.py [-h] [yaml_files ...] [--<ARG_NAME> <ARG_VALUE> ...]"
+epilog = ("  --<ARG_NAME> specify additional CLI ars to be passed to the binary (override all preset files). "
+          "Unknown args will be ignored.")
+
+parser = argparse.ArgumentParser(
+    description=description, usage=usage, epilog=epilog, formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument("yaml_files", nargs="*",
+                    help="Arbitrary number of YAML files from which to read preset values. "
+                    "If two files specify the same values the later one will be used.")
+
+known_args, unknown_args = parser.parse_known_args()
+
+if not known_args.yaml_files and not unknown_args:
+    parser.print_help()
+    sys.exit(0)
+
+props = dict()
+
+for yaml_file in known_args.yaml_files:
+    with open(yaml_file, "r") as f:
+        props.update(yaml.load(f, yaml.SafeLoader))
 
 props = {prop.replace("_", "-"): val for prop, val in props.items()}
 
@@ -61,7 +93,7 @@ for cli_arg in CLI_ARGS_MAIN:
     if value != "True":
         command_list.append(str(value))
 
-print(command_list)
+command_list += unknown_args
 
 sp = subprocess.Popen(command_list)
 
