ef7030a50fa24b3ea66d44933a51e0039a10bf8c
JohannesGaessler <johannesg@5d6.de>
2024-01-01 13:11:08 +0100

    abs col max

--- llama.cpp
+++ llama.cpp
@@ -8889,7 +8889,7 @@ static ggml_type get_k_quant_type(quantize_state_internal & qs, ggml_type new_ty
 
 static void llama_model_quantize_internal(const std::string & fname_inp, const std::string & fname_out, const llama_model_quantize_params * params) {
     float   * data_reordered = (float   *) std::malloc(1024*1024*1024);
-    float   * col_sums       = (float *)   std::malloc(1024*1024);
+    float   * col_values       = (float *)   std::malloc(1024*1024);
     int64_t * sorting        = (int64_t *) std::malloc(1024*1024*1024);
     for (int i = 0; i < 128; ++i) {
         sorting[i*1024*1024] = -1;
@@ -9095,22 +9095,27 @@ static void llama_model_quantize_internal(const std::string & fname_inp, const s
                 }
             } else if (name.size() >= 15 && strcmp(name.data() + name.size() - 15, "ffn_down.weight") == 0) {
                 for (int col = 0; col < ne0; ++col) {
-                    col_sums[col] = 0.0f;
+                    col_values[col] = 0.0f;
                     for (int row = 0; row < ne1; ++row) {
-                        col_sums[col] += fabsf(f32_data[row*ne0 + col]);
+                        // col_values[col] += fabsf(f32_data[row*ne0 + col]); // abs col sum
+
+                        // abs col max
+                        if (fabsf(f32_data[row*ne0 + col]) > col_values[col]) {
+                            col_values[col] = fabsf(f32_data[row*ne0 + col]);
+                        }
                     }
                 }
                 for (int col = 0; col < ne0; ++col) {
                     float   value_min = 1e9;
                     int64_t index_min = 0;
                     for (int col2 = 0; col2 < ne0; ++ col2) {
-                        if (col_sums[col2] < value_min) {
-                            value_min = col_sums[col2];
+                        if (col_values[col2] < value_min) {
+                            value_min = col_values[col2];
                             index_min = col2;
                         }
                     }
                     sorting[il*1024*1024 + col] = index_min;
-                    col_sums[index_min] = 1e9;
+                    col_values[index_min] = 1e9;
                 }
                 for (int row = 0; row < ne1; ++row) {
                     for (int col = 0; col < ne0; ++col) {
