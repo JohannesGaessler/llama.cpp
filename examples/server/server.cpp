70a3d8502c894a339db951b57afda7d8b4f9b3eb
Johannes Gäßler <johannesg@5d6.de>
2024-05-02 13:46:03 +0200

    disable ngram_cache calls for n_draft == 0

--- examples/server/server.cpp
+++ examples/server/server.cpp
@@ -1079,10 +1079,12 @@ struct server_context {
             for (int i = 0; i < (int)system_tokens.size(); ++i) {
                 llama_batch_add(batch, system_tokens[i], i, { 0 }, false);
             }
-            for (auto slot : slots) {
-                memcpy(slot.context_tokens.data(), system_tokens.data(), system_tokens.size()*sizeof(llama_token));
-                llama_ngram_cache_update(
-                    slot.nc_context, LLAMA_NGRAM_MIN, LLAMA_NGRAM_MAX, system_tokens.data(), system_tokens.size(), system_tokens.size(), false);
+            if (n_draft > 0) {
+                for (auto slot : slots) {
+                    memcpy(slot.context_tokens.data(), system_tokens.data(), system_tokens.size()*sizeof(llama_token));
+                    llama_ngram_cache_update(
+                        slot.nc_context, LLAMA_NGRAM_MIN, LLAMA_NGRAM_MAX, system_tokens.data(), system_tokens.size(), system_tokens.size(), false);
+                }
             }
 
             const int32_t n_batch = llama_n_batch(ctx);
@@ -1794,7 +1796,9 @@ struct server_context {
             if (slot.command == SLOT_COMMAND_RELEASE) {
                 slot.state       = SLOT_STATE_IDLE;
                 slot.command     = SLOT_COMMAND_NONE;
-                llama_ngram_cache_merge(nc_dynamic, slot.nc_context);
+                if (n_draft > 0) {
+                    llama_ngram_cache_merge(nc_dynamic, slot.nc_context);
+                }
                 slot.t_last_used = ggml_time_us();
 
                 LOG_INFO("slot released", {
@@ -1902,8 +1906,11 @@ struct server_context {
             //       this is not great and needs to be improved somehow
             llama_batch_add(batch, slot.sampled, system_tokens.size() + slot_npast, { slot.id + 1 }, true);
             slot.context_tokens[system_tokens.size() + slot_npast] = slot.sampled;
-            llama_ngram_cache_update(
-                slot.nc_context, LLAMA_NGRAM_MIN, LLAMA_NGRAM_MAX, slot.context_tokens.data(), system_tokens.size() + slot_npast, 1, false);
+            if (n_draft > 0) {
+                llama_ngram_cache_update(
+                    slot.nc_context, LLAMA_NGRAM_MIN, LLAMA_NGRAM_MAX,
+                    slot.context_tokens.data(), system_tokens.size() + slot_npast, 1, false);
+            }
 
             slot.n_past += 1;
 
@@ -2153,8 +2160,10 @@ struct server_context {
 
                         llama_batch_add(batch, prompt_tokens[slot.n_past], system_tokens.size() + slot_npast, { slot.id + 1 }, false);
                         slot.context_tokens[system_tokens.size() + slot_npast] = prompt_tokens[slot.n_past];
-                        llama_ngram_cache_update(
-                            slot.nc_context, LLAMA_NGRAM_MIN, LLAMA_NGRAM_MAX, slot.context_tokens.data(), slot_npast, 1, false);
+                        if (n_draft > 0) {
+                            llama_ngram_cache_update(
+                                slot.nc_context, LLAMA_NGRAM_MIN, LLAMA_NGRAM_MAX, slot.context_tokens.data(), slot_npast, 1, false);
+                        }
 
                         if (slot.params.cache_prompt) {
                             slot.cache_tokens.push_back(prompt_tokens[slot.n_past]);
