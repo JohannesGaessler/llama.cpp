b05516e81ea5056339f3e9d702a827b989099755
Johannes Gäßler <johannesg@5d6.de>
2024-05-24 15:52:46 +0200

    q5_1 K

--- ggml-cuda/fattn-common.cuh
+++ ggml-cuda/fattn-common.cuh
@@ -139,6 +139,42 @@ static __device__ __forceinline__ half vec_dot_fattn_vec_KQ_q5_0(
 }
 
 template<int D>
+static __device__ __forceinline__ half vec_dot_fattn_vec_KQ_q5_1(
+    const char * __restrict__ K_c, const half2 * __restrict__ Q_h2, const int * __restrict__ Q_q8, const half2 * __restrict__ Q_ds) {
+
+    const block_q5_1 * K_q5_1 = (const block_q5_1 *) K_c;
+    GGML_UNUSED(Q_h2);
+
+    half sum = 0.0f;
+
+#pragma unroll
+    for (int k_KQ_0 = 0; k_KQ_0 < D/sizeof(int); k_KQ_0 += WARP_SIZE) {
+        const int k_KQ = k_KQ_0 + threadIdx.x;
+
+        const int ib    = k_KQ /  QI8_1;
+        const int iqs4  = k_KQ %  QI5_1;
+        const int iqs8  = k_KQ %  QI8_1;
+        const int shift = k_KQ & (QI8_1/2);
+
+        int v = (get_int_from_uint8(K_q5_1[ib].qs, iqs4) >> shift) & 0x0F0F0F0F;
+        const int vh = get_int_from_uint8(K_q5_1[ib].qh, 0) >> (iqs8 * QI5_1);
+        v |= (vh <<  4) & 0x00000010; // 0 ->  4
+        v |= (vh << 11) & 0x00001000; // 1 -> 12
+        v |= (vh << 18) & 0x00100000; // 2 -> 20
+        v |= (vh << 25) & 0x10000000; // 3 -> 28
+
+        const int u = Q_q8[k_KQ_0/WARP_SIZE];
+
+        const int   sumi = __dp4a(v, u, 0);
+        const half2 d5d8_m5s8 = K_q5_1[ib].dm * Q_ds[k_KQ_0/WARP_SIZE];
+        const half2 sumid5d8_m5s8scaled = d5d8_m5s8 * make_half2(sumi, 1.0f/QI8_1);
+        sum += __low2half(sumid5d8_m5s8scaled) + __high2half(sumid5d8_m5s8scaled);
+    }
+
+    return sum;
+}
+
+template<int D>
 static __device__ __forceinline__ half vec_dot_fattn_vec_KQ_q8_0(
     const char * __restrict__ K_c, const half2 * __restrict__ Q_h2, const int * __restrict__ Q_q8, const half2 * __restrict__ Q_ds) {
 
