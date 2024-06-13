76e2cb016ba401f01530b5530ac2b28c0b784e31
Johannes Gäßler <johannesg@5d6.de>
2024-06-13 13:32:48 +0200

    WIP

--- ggml-cuda/mmq.cuh
+++ ggml-cuda/mmq.cuh
@@ -829,14 +829,14 @@ template <int mmq_y, int nwarps, bool need_check> static __device__ __forceinlin
 
 #pragma unroll
         for (int l = 0; l < QR3_K; ++l) {
-            const int k = kbx*(QR2_K*QI2_K) + (kqsx/8)*32 + l*8 + kqsx % 8;
+            const int k = kbx*QI2_K + (kqsx/8)*8 + l*2 + (kqsx % 8)/4;
 
-            int x_qs_k = ((x_ql_0 >> (2*l)) & 0x03030303) << (2*(k % 4));
+            int x_qs_k = ((x_ql_0 >> (2*l)) & 0x03030303) << (2*(kqsx % 4));
             x_qs_k |= __shfl_xor_sync(0xFFFFFFFF, x_qs_k, 1, WARP_SIZE);
             x_qs_k |= __shfl_xor_sync(0xFFFFFFFF, x_qs_k, 2, WARP_SIZE);
 
-            if (k % 4 == 0) {
-                x_qs[i*(WARP_SIZE + 1) + k/4] = x_qs_k;
+            if (kqsx % 4 == 0) {
+                x_qs[i*(WARP_SIZE + 1) + k] = x_qs_k;
             }
         }
 
