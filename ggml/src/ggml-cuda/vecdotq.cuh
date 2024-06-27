ed85d09f6c751f0ad8d44a444fd897eb5f546a5a
Johannes Gäßler <johannesg@5d6.de>
2024-06-27 21:46:48 +0200

    WIP

--- ggml/src/ggml-cuda/vecdotq.cuh
+++ ggml/src/ggml-cuda/vecdotq.cuh
@@ -841,9 +841,8 @@ static __device__ __forceinline__ float vec_dot_iq2_xxs_q8_1(
 
     const int q2 = get_int_b2(bq2->qs, iqs);
     const uint8_t * aux8 = (const uint8_t *) &q2;
-    const int8_t  * q8   = bq8_1[iqs/2].qs;
-
     const uint aux32 = get_int_b2(bq2->qs, iqs + 1);
+
     int sumi = 0;
 #pragma unroll
     for (int k0 = 0; k0 < 8; k0 += 2) {
@@ -855,16 +854,16 @@ static __device__ __forceinline__ float vec_dot_iq2_xxs_q8_1(
         const int signs0 = ((signs_packed & 0x03) << 7) | ((signs_packed & 0x0C) << 21);
         const int mask0 = __vcmpeq4(signs0, 0x00000000);
         const int grid0 = (grid_pos[0] & mask0) | (grid_neg.x & (~mask0));
-        const int u0 = get_int_from_int8_aligned(q8, k0 + 0);
+        const int u0 = get_int_from_int8_aligned(bq8_1[iqs/2].qs, k0 + 0);
         sumi = __dp4a(grid0, u0, sumi);
 
         const int signs1 = ((signs_packed & 0x30) << 3) | ((signs_packed & 0xC0) << 17);
         const int mask1 = __vcmpeq4(signs1, 0x00000000);
         const int grid1 = (grid_pos[1] & mask1) | (grid_neg.y & (~mask1));
-        const int u1 = get_int_from_int8_aligned(q8, k0 + 1);
+        const int u1 = get_int_from_int8_aligned(bq8_1[iqs/2].qs, k0 + 1);
         sumi = __dp4a(grid1, u1, sumi);
     }
-    const float d = (float)bq2->d * (0.5f + (aux32 >> 28)) * __low2float(bq8_1[iqs/2].ds) * 0.25f;
+    const float d = __half2float(bq2->d) * (0.5f + (aux32 >> 28)) * __low2float(bq8_1[iqs/2].ds) / 4;
     return d * sumi;
 }
 
