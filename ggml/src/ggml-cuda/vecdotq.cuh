c952edd241033bb63d8c3b6e7e18ebd9812f2ee1
Johannes Gäßler <johannesg@5d6.de>
2024-06-28 23:30:51 +0200

    WIP

--- ggml/src/ggml-cuda/vecdotq.cuh
+++ ggml/src/ggml-cuda/vecdotq.cuh
@@ -1103,23 +1103,23 @@ static __device__ __forceinline__ float vec_dot_iq1_m_q8_1(
     float sumf[2] = {0.0f};
 #if __CUDA_ARCH__ >= MIN_CC_DP4A // lowest compute capability for integer intrinsics
 #pragma unroll
-    for (int l = 0; l < 4; ++l) {
-        const int * grid = (const int *)(iq1s_grid_gpu + (bq1->qs[4*iqs+l] | (((bq1->qh[2*iqs+l/2] >> 4*(l%2)) & 7) << 8)));
+    for (int l0 = 0; l0 < 8; l0 += 2) {
+        const int * grid = (const int *)(iq1s_grid_gpu + (bq1->qs[4*iqs + l0/2] | (((bq1->qh[2*iqs + l0/4] >> 4*((l0/2)%2)) & 7) << 8)));
 
         const int grid0 = grid[0] & 0x0f0f0f0f;
         const int grid1 = (grid[0] >> 4) & 0x0f0f0f0f;
 
-        const int u0 = get_int_b4(bq8_1[iqs].qs, 2*l + 0);
-        const int u1 = get_int_b4(bq8_1[iqs].qs, 2*l + 1);
+        const int u0 = get_int_b4(bq8_1[iqs].qs, l0 + 0);
+        const int u1 = get_int_b4(bq8_1[iqs].qs, l0 + 1);
 
-        sumi[l/2] = __dp4a(grid0, u0, sumi[l/2]);
-        sumi[l/2] = __dp4a(grid1, u1, sumi[l/2]);
-        const float delta = (bq1->qh[2*iqs+l/2] >> 4*(l%2)) & 0x08 ? -1-IQ1M_DELTA : -1+IQ1M_DELTA;
+        sumi[l0/4] = __dp4a(grid0, u0, sumi[l0/4]);
+        sumi[l0/4] = __dp4a(grid1, u1, sumi[l0/4]);
+        const float delta = (bq1->qh[2*iqs + l0/4] >> 4*((l0/2)%2)) & 0x08 ? -1-IQ1M_DELTA : -1+IQ1M_DELTA;
 
         int sumy = 0;
         sumy = __dp4a(u0, 0x01010101, sumy);
         sumy = __dp4a(u1, 0x01010101, sumy);
-        sumf[l/2] += delta*sumy;
+        sumf[l0/4] += delta*sumy;
     }
 #else
     const int8_t * q8 = bq8_1[iqs].qs;
