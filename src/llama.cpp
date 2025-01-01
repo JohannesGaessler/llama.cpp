35aafca90af01303d38a50da84bb1403d4f431e1
Johannes Gäßler <johannesg@5d6.de>
2025-01-01 17:14:31 +0100

    fixup

--- src/llama.cpp
+++ src/llama.cpp
@@ -1874,7 +1874,7 @@ static std::string gguf_kv_to_str(const struct gguf_context * ctx_gguf, int i) {
             {
                 const enum gguf_type arr_type = gguf_get_arr_type(ctx_gguf, i);
                 int arr_n = gguf_get_arr_n(ctx_gguf, i);
-                const void * data = gguf_get_arr_data(ctx_gguf, i);
+                const void * data = arr_type == GGUF_TYPE_STRING ? nullptr : gguf_get_arr_data(ctx_gguf, i);
                 std::stringstream ss;
                 ss << "[";
                 for (int j = 0; j < arr_n; j++) {
