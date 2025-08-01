854497351af9b8e529211b967867eb571ce228e0
Johannes Gäßler <johannesg@5d6.de>
2025-08-01 19:15:49 +0200

    WIP

--- scripts/server-bench-batch.sh
+++ scripts/server-bench-batch.sh
@@ -3,10 +3,10 @@
 export LLAMA_ARG_N_PARALLEL=1
 export name=vllm
 
-for n_prompt in 2048 4096 8196 12288 16376 24576; do
+for n_prompt in 2048 4096 6144 8196 10240 12288 14336 16384 18432 20480 22528 24576; do
     for n_predict in 256 512 768 1024; do
         echo n_prompt=$n_prompt n_predict=$n_predict
-        echo python3 server-bench.py \
+        python3 server-bench.py \
             --path_server "http://0.0.0.0:${PORT}" \
             --name $name \
             --path_db server-bench.sqlite \
