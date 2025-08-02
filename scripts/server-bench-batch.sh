a01acee866ee0335bd7f47ae6439028b1b4bf77e
Johannes Gäßler <johannesg@5d6.de>
2025-08-02 20:05:50 +0200

    WIP

--- scripts/server-bench-batch.sh
+++ scripts/server-bench-batch.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-export name=llama.cpp
+export name=vllm
 
-for n_parallel in 16; do
+for n_parallel in 1 2 4 8 16; do
     export LLAMA_ARG_N_PARALLEL=$n_parallel
     # for n_prompt in 4096 6144 10240 12288 14336 18432 20480 22528; do
     for n_prompt in 2048 24576 8192 16384; do
@@ -12,7 +12,7 @@ for n_parallel in 16; do
                 --path_server "http://0.0.0.0:${PORT}" \
                 --name $name \
                 --path_db server-bench.sqlite \
-                --n_prompt 32 \
+                --n_prompt $((32 * $n_parallel)) \
                 --prompt_source rng-${n_prompt}-${n_prompt} \
                 --n_predict_min $n_predict --n_predict $n_predict \
                 --seed_offset -1
