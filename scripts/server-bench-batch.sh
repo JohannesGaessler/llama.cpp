35682842ce7e1740dc2fd0f14a8b9d70eafe5c38
Johannes Gäßler <johannesg@5d6.de>
2025-08-02 19:39:34 +0200

    WIP

--- scripts/server-bench-batch.sh
+++ scripts/server-bench-batch.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-export name=vllm
+export name=llama.cpp
 
-for n_parallel in 2 4 8 16; do
+for n_parallel in 16; do
     export LLAMA_ARG_N_PARALLEL=$n_parallel
     # for n_prompt in 4096 6144 10240 12288 14336 18432 20480 22528; do
     for n_prompt in 2048 24576 8192 16384; do
