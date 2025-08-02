b71fd59b596c2a422c0c3094f80ffc7917d45dec
Johannes Gäßler <johannesg@5d6.de>
2025-08-02 17:09:36 +0200

    WIP

--- scripts/server-bench-batch.sh
+++ scripts/server-bench-batch.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env sh
 
-export LLAMA_ARG_N_PARALLEL=1
-export name=llama.cpp
+export name=vllm
 
-for n_prompt in 2048 24576 8192 16384 4096 6144 10240 12288 14336 18432 20480 22528; do
-    for n_predict in 256 512 768 1024; do
-        echo n_prompt=$n_prompt n_predict=$n_predict
-        python3 server-bench.py \
-            --path_server "http://0.0.0.0:${PORT}" \
-            --name $name \
-            --path_db server-bench.sqlite \
-            --n_prompt $((32*$LLAMA_ARG_N_PARALLEL)) \
-            --prompt_source rng-${n_prompt}-${n_prompt} \
-            --n_predict_min $n_predict --n_predict $n_predict \
-            --seed_offset -1
+for n_parallel in 2 4 8 16; do
+    export LLAMA_ARG_N_PARALLEL=$n_parallel
+    # for n_prompt in 4096 6144 10240 12288 14336 18432 20480 22528; do
+    for n_prompt in 2048 24576 8192 16384; do
+        for n_predict in 256 512 768 1024; do
+            echo n_parallel=$n_parallel n_prompt=$n_prompt n_predict=$n_predict
+            python3 server-bench.py \
+                --path_server "http://0.0.0.0:${PORT}" \
+                --name $name \
+                --path_db server-bench.sqlite \
+                --n_prompt 32 \
+                --prompt_source rng-${n_prompt}-${n_prompt} \
+                --n_predict_min $n_predict --n_predict $n_predict \
+                --seed_offset -1
+        done
     done
 done
