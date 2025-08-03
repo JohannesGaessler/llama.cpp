38f300c9bc6ffd9badc10ffb87b10eaddc109644
Johannes Gäßler <johannesg@5d6.de>
2025-08-03 21:20:12 +0200

    WIP

--- scripts/server-bench-batch.sh
+++ scripts/server-bench-batch.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
-export name=llama.cpp
+export name=vllm
 
 for n_parallel in 16; do
     export LLAMA_ARG_N_PARALLEL=$n_parallel
-    # for n_prompt in 4096 6144 10240 12288 14336 18432 20480 22528; do
-    for n_prompt in 2048 24576 8192 16384; do
+    for n_prompt in 4096 6144 10240 12288 14336 18432 20480 22528; do
+    # for n_prompt in 2048 24576 8192 16384; do
         for n_predict in 256 512 768 1024; do
             echo n_parallel=$n_parallel n_prompt=$n_prompt n_predict=$n_predict
             python3 server-bench.py \
