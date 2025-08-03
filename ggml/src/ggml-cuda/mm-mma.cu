a4ac54b4fab09a7a6f0f2699e87e86b20ec291ba
Johannes Gäßler <johannesg@5d6.de>
2025-08-03 23:57:15 +0200

    WIP

--- ggml/src/ggml-cuda/mm-mma.cu
+++ ggml/src/ggml-cuda/mm-mma.cu
@@ -5,25 +5,23 @@
 
 using namespace ggml_cuda_mma;
 
-typedef tile<16,  8, half2> tile_A;
-typedef tile< 8,  8, half2> tile_B;
-typedef tile<16,  8, half2> tile_B_16;
-typedef tile<16,  8, float> tile_C;
-typedef tile<16, 16, float> tile_C_16;
+typedef tile<16, 8, half2> tile_A;
+typedef tile< 8, 8, half2> tile_B;
+typedef tile<16, 8, float> tile_C;
 
-template <typename T, typename type_acc, int ncols_dst, int nwarps>
+template <typename T, typename type_acc, int rows_per_block, int cols_per_block, int nwarps>
 __launch_bounds__(ggml_cuda_get_physical_warp_size()*nwarps, 1)
-static __global__ void mul_mat_mma(
+static __global__ void mul_mat_f(
         const T * __restrict__ x, const float * __restrict__ y, const int32_t * __restrict__ ids, float * __restrict__ dst,
         const int ncols2, const int nchannels_y, const int stride_row, const int stride_col_y2, const int stride_col_dst,
         const int channel_ratio, const int stride_channel_x, const int stride_channel_y, const int stride_channel_dst,
         const int sample_ratio, const int stride_sample_x, const int stride_sample_y, const int stride_sample_dst) {
-    constexpr int nrows = 32;
     constexpr int warp_size = ggml_cuda_get_physical_warp_size();
     constexpr int tile_k_padded = warp_size + 4;
-    constexpr int ntA = nrows / tile_A::I;
+    constexpr int ntA = rows_per_block / tile_A::I;
+    constexpr int ntB = cols_per_block / tile_B::I;
 
-    const int row0        = blockIdx.x * nrows;
+    const int row0        = blockIdx.x * rows_per_block;
     const int channel_dst = blockIdx.y;
     const int channel_x   = channel_dst / channel_ratio;
     const int channel_y   = channel_dst;
@@ -39,7 +37,7 @@ static __global__ void mul_mat_mma(
 
     extern __shared__ char data_mmv[];
 
-    tile_C_16 C[ntA];
+    tile_C C[ntA][ntB];
 
     if constexpr (std::is_same<T, half>::value) {
         half2 * tile_xy = (half2 *) data_mmv + threadIdx.y*(tile_A::I * tile_k_padded);
@@ -60,17 +58,20 @@ static __global__ void mul_mat_mma(
             }
 
 #pragma unroll
-            for (int j = 0; j < tile_B_16::I; ++j) {
-                const float2 tmp = y2[j*stride_col_y2 + col2];
-                tile_xy[j*tile_k_padded + threadIdx.x] = make_half2(tmp.x, tmp.y);
-            }
+            for (int itB = 0; itB < ntB; ++itB) {
 #pragma unroll
-            for (int k0 = 0; k0 < warp_size; k0 += tile_B_16::J) {
-                tile_B_16 B;
-                load_ldmatrix(B, tile_xy + k0, tile_k_padded);
+                for (int j = 0; j < tile_B::I; ++j) {
+                    const float2 tmp = y2[(itB*tile_B::I + j)*stride_col_y2 + col2];
+                    tile_xy[j*tile_k_padded + threadIdx.x] = make_half2(tmp.x, tmp.y);
+                }
 #pragma unroll
-                for (int itA = 0; itA < ntA; ++itA) {
-                    mma(C[itA], B, A[itA][k0/tile_B_16::J]);
+                for (int k0 = 0; k0 < warp_size; k0 += tile_B::J) {
+                    tile_B B;
+                    load_ldmatrix(B, tile_xy + k0, tile_k_padded);
+#pragma unroll
+                    for (int itA = 0; itA < ntA; ++itA) {
+                        mma(C[itA][itB], A[itA][k0/tile_B::J], B);
+                    }
                 }
             }
         }
@@ -79,32 +80,40 @@ static __global__ void mul_mat_mma(
     }
 
     float * buf_iw = (float *) data_mmv;
-    constexpr int kiw = nwarps*nrows + 1;
+    constexpr int kiw = nwarps*rows_per_block + 4;
 
     if (nwarps > 1) {
         __syncthreads();
     }
 #pragma unroll
-    for (int itA = 0; itA < ntA; ++itA) {
+    for (int itB = 0; itB < ntB; ++itB) {
 #pragma unroll
-        for (int l = 0; l < tile_C_16::ne; ++l) {
-            buf_iw[tile_C_16::get_i(l)*kiw + threadIdx.y*nrows + itA*tile_C_16::J + tile_C_16::get_j(l)] = C[itA].x[l];
+        for (int itA = 0; itA < ntA; ++itA) {
+#pragma unroll
+            for (int l = 0; l < tile_C::ne; ++l) {
+                const int i = threadIdx.y*rows_per_block + itA*tile_C::I + tile_C::get_i(l);
+                const int j = itB*tile_C::J + tile_C::get_j(l);
+                buf_iw[j*kiw + i] = C[itA][itB].x[l];
+            }
         }
     }
 
     if (nwarps > 1) {
         __syncthreads();
-        if (threadIdx.y > 0) {
+    }
+
+#pragma unroll
+    for (int j0 = 0; j0 < cols_per_block; j0 += nwarps) {
+        const int j = j0 + threadIdx.y;
+
+        if (j0 + nwarps > cols_per_block && j >= cols_per_block) {
             return;
         }
-    }
 
-    static_assert(nrows == 32 && warp_size == 32, "need loop/check");
-#pragma unroll
-    for (int j = 0; j < tile_C_16::I; ++j) {
         float sum = 0.0f;
+        static_assert(rows_per_block == warp_size, "need loop/check");
 #pragma unroll
-        for (int i0 = 0; i0 < nwarps*nrows; i0 += nrows) {
+        for (int i0 = 0; i0 < nwarps*rows_per_block; i0 += rows_per_block) {
             const int i = i0 + threadIdx.x;
 
             sum += buf_iw[j*kiw + i];
@@ -113,16 +122,16 @@ static __global__ void mul_mat_mma(
     }
 }
 
-template <typename T, typename type_acc, int ncols_dst>
+template <typename T, typename type_acc, int cols_per_block>
 static void mul_mat_mma_cuda(
         const T * x, const float * y, const int32_t * ids, float * dst,
-        const int64_t ncols, const int64_t nrows,
+        const int64_t ncols_x, const int64_t nrows_x,
         const int64_t stride_row, const int64_t stride_col_y, const int64_t stride_col_dst,
         const int64_t nchannels_x, const int64_t nchannels_y, const int64_t nchannels_dst,
         const int64_t stride_channel_x, const int64_t stride_channel_y, const int64_t stride_channel_dst, const int64_t nsamples_x,
         const int64_t nsamples_dst, const int64_t stride_sample_x, const int64_t stride_sample_y, const int64_t stride_sample_dst,
         cudaStream_t stream) {
-    GGML_ASSERT(ncols        % 2 == 0);
+    GGML_ASSERT(ncols_x      % 2 == 0);
     GGML_ASSERT(stride_row   % 2 == 0);
     GGML_ASSERT(stride_col_y % 2 == 0);
     GGML_ASSERT(ids || nchannels_dst % nchannels_x == 0);
@@ -134,68 +143,68 @@ static void mul_mat_mma_cuda(
     const int warp_size = ggml_cuda_info().devices[device].warp_size;
 
     int64_t nwarps_best     = 1;
-    int64_t niter_best      = (ncols + warp_size*2 - 1) / (warp_size*2);
+    int64_t niter_best      = (ncols_x + warp_size*2 - 1) / (warp_size*2);
     int64_t max_block_size  = 256;
     for (int64_t nwarps = 2; nwarps <= max_block_size/warp_size; nwarps++) {
-        const int64_t niter = (ncols + nwarps*warp_size*2 - 1) / (nwarps*warp_size*2);
+        const int64_t niter = (ncols_x + nwarps*warp_size*2 - 1) / (nwarps*warp_size*2);
         if (niter < niter_best) {
             niter_best  = niter;
             nwarps_best = nwarps;
         }
     }
 
-    constexpr int nrows_kernel = 32;
+    constexpr int rows_per_block = 32;
     const int nbytes_shared_iter = nwarps_best * tile_A::I * (warp_size + 4) * 4;
-    const int nbytes_shared_combine = tile_C_16::I * (nwarps_best*nrows_kernel + 1) * 4;
+    const int nbytes_shared_combine = tile_C::I * (nwarps_best*rows_per_block + 4) * 4;
     const int nbytes_shared = std::max(nbytes_shared_iter, nbytes_shared_combine);
-    const dim3 block_nums(nrows/nrows_kernel, nchannels_dst, nsamples_dst);
+    const dim3 block_nums(nrows_x/rows_per_block, nchannels_dst, nsamples_dst);
     const dim3 block_dims(warp_size, nwarps_best, 1);
     switch (nwarps_best) {
         case 1: {
-            mul_mat_mma<T, type_acc, ncols_dst, 1><<<block_nums, block_dims, nbytes_shared, stream>>>
-                (x, y, ids, dst, ncols/2, nchannels_y, stride_row, stride_col_y/2, stride_col_dst,
+            mul_mat_f<T, type_acc, rows_per_block, cols_per_block, 1><<<block_nums, block_dims, nbytes_shared, stream>>>
+                (x, y, ids, dst, ncols_x/2, nchannels_y, stride_row, stride_col_y/2, stride_col_dst,
                  channel_ratio, stride_channel_x, stride_channel_y, stride_channel_dst,
                  sample_ratio, stride_sample_x, stride_sample_y, stride_sample_dst);
         } break;
         case 2: {
-            mul_mat_mma<T, type_acc, ncols_dst, 2><<<block_nums, block_dims, nbytes_shared, stream>>>
-                (x, y, ids, dst, ncols/2, nchannels_y, stride_row, stride_col_y/2, stride_col_dst,
+            mul_mat_f<T, type_acc, rows_per_block, cols_per_block, 2><<<block_nums, block_dims, nbytes_shared, stream>>>
+                (x, y, ids, dst, ncols_x/2, nchannels_y, stride_row, stride_col_y/2, stride_col_dst,
                  channel_ratio, stride_channel_x, stride_channel_y, stride_channel_dst,
                  sample_ratio, stride_sample_x, stride_sample_y, stride_sample_dst);
         } break;
         case 3: {
-            mul_mat_mma<T, type_acc, ncols_dst, 3><<<block_nums, block_dims, nbytes_shared, stream>>>
-                (x, y, ids, dst, ncols/2, nchannels_y, stride_row, stride_col_y/2, stride_col_dst,
+            mul_mat_f<T, type_acc, rows_per_block, cols_per_block, 3><<<block_nums, block_dims, nbytes_shared, stream>>>
+                (x, y, ids, dst, ncols_x/2, nchannels_y, stride_row, stride_col_y/2, stride_col_dst,
                  channel_ratio, stride_channel_x, stride_channel_y, stride_channel_dst,
                  sample_ratio, stride_sample_x, stride_sample_y, stride_sample_dst);
         } break;
         case 4: {
-            mul_mat_mma<T, type_acc, ncols_dst, 4><<<block_nums, block_dims, nbytes_shared, stream>>>
-                (x, y, ids, dst, ncols/2, nchannels_y, stride_row, stride_col_y/2, stride_col_dst,
+            mul_mat_f<T, type_acc, rows_per_block, cols_per_block, 4><<<block_nums, block_dims, nbytes_shared, stream>>>
+                (x, y, ids, dst, ncols_x/2, nchannels_y, stride_row, stride_col_y/2, stride_col_dst,
                  channel_ratio, stride_channel_x, stride_channel_y, stride_channel_dst,
                  sample_ratio, stride_sample_x, stride_sample_y, stride_sample_dst);
         } break;
         case 5: {
-            mul_mat_mma<T, type_acc, ncols_dst, 5><<<block_nums, block_dims, nbytes_shared, stream>>>
-                (x, y, ids, dst, ncols/2, nchannels_y, stride_row, stride_col_y/2, stride_col_dst,
+            mul_mat_f<T, type_acc, rows_per_block, cols_per_block, 5><<<block_nums, block_dims, nbytes_shared, stream>>>
+                (x, y, ids, dst, ncols_x/2, nchannels_y, stride_row, stride_col_y/2, stride_col_dst,
                  channel_ratio, stride_channel_x, stride_channel_y, stride_channel_dst,
                  sample_ratio, stride_sample_x, stride_sample_y, stride_sample_dst);
         } break;
         case 6: {
-            mul_mat_mma<T, type_acc, ncols_dst, 6><<<block_nums, block_dims, nbytes_shared, stream>>>
-                (x, y, ids, dst, ncols/2, nchannels_y, stride_row, stride_col_y/2, stride_col_dst,
+            mul_mat_f<T, type_acc, rows_per_block, cols_per_block, 6><<<block_nums, block_dims, nbytes_shared, stream>>>
+                (x, y, ids, dst, ncols_x/2, nchannels_y, stride_row, stride_col_y/2, stride_col_dst,
                  channel_ratio, stride_channel_x, stride_channel_y, stride_channel_dst,
                  sample_ratio, stride_sample_x, stride_sample_y, stride_sample_dst);
         } break;
         case 7: {
-            mul_mat_mma<T, type_acc, ncols_dst, 7><<<block_nums, block_dims, nbytes_shared, stream>>>
-                (x, y, ids, dst, ncols/2, nchannels_y, stride_row, stride_col_y/2, stride_col_dst,
+            mul_mat_f<T, type_acc, rows_per_block, cols_per_block, 7><<<block_nums, block_dims, nbytes_shared, stream>>>
+                (x, y, ids, dst, ncols_x/2, nchannels_y, stride_row, stride_col_y/2, stride_col_dst,
                  channel_ratio, stride_channel_x, stride_channel_y, stride_channel_dst,
                  sample_ratio, stride_sample_x, stride_sample_y, stride_sample_dst);
         } break;
         case 8: {
-            mul_mat_mma<T, type_acc, ncols_dst, 8><<<block_nums, block_dims, nbytes_shared, stream>>>
-                (x, y, ids, dst, ncols/2, nchannels_y, stride_row, stride_col_y/2, stride_col_dst,
+            mul_mat_f<T, type_acc, rows_per_block, cols_per_block, 8><<<block_nums, block_dims, nbytes_shared, stream>>>
+                (x, y, ids, dst, ncols_x/2, nchannels_y, stride_row, stride_col_y/2, stride_col_dst,
                  channel_ratio, stride_channel_x, stride_channel_y, stride_channel_dst,
                  sample_ratio, stride_sample_x, stride_sample_y, stride_sample_dst);
         } break;
@@ -203,6 +212,28 @@ static void mul_mat_mma_cuda(
             GGML_ABORT("fatal error");
         } break;
     }
+}
+
+template <typename T, typename type_acc>
+static void mul_mat_f_switch_cols_per_block(
+        const T * x, const float * y, const int32_t * ids, float * dst,
+        const int64_t ncols_x, const int64_t nrows_x, const int64_t ncols_dst,
+        const int64_t stride_row, const int64_t stride_col_y, const int64_t stride_col_dst,
+        const int64_t nchannels_x, const int64_t nchannels_y, const int64_t nchannels_dst,
+        const int64_t stride_channel_x, const int64_t stride_channel_y, const int64_t stride_channel_dst, const int64_t nsamples_x,
+        const int64_t nsamples_dst, const int64_t stride_sample_x, const int64_t stride_sample_y, const int64_t stride_sample_dst,
+        cudaStream_t stream) {
+    if (ncols_dst <= 8) {
+        mul_mat_mma_cuda<half, float,  8>(x, y, ids, dst, ncols_x, nrows_x, stride_row, stride_col_y, stride_col_dst,
+            nchannels_x, nchannels_y,  nchannels_dst, stride_channel_x, stride_channel_y, stride_channel_dst,
+            nsamples_x,                nsamples_dst,  stride_sample_x,  stride_sample_y,  stride_sample_dst,  stream);
+        return;
+    }
+    GGML_ASSERT(ncols_dst <= 16);
+    mul_mat_mma_cuda<half, float, 16>(x, y, ids, dst, ncols_x, nrows_x, stride_row, stride_col_y, stride_col_dst,
+        nchannels_x, nchannels_y,  nchannels_dst, stride_channel_x, stride_channel_y, stride_channel_dst,
+        nsamples_x,                nsamples_dst,  stride_sample_x,  stride_sample_y,  stride_sample_dst,  stream);
+    return;
 }
 
 void ggml_cuda_mul_mat_mma(ggml_backend_cuda_context & ctx, const ggml_tensor * src0, const ggml_tensor * src1, const ggml_tensor * ids, ggml_tensor * dst) {
@@ -252,7 +283,7 @@ void ggml_cuda_mul_mat_mma(ggml_backend_cuda_context & ctx, const ggml_tensor * 
     switch (src0->type) {
         case GGML_TYPE_F16: {
             const half * src0_d = (const half *) src0->data;
-            mul_mat_mma_cuda<half, float, 16>(src0_d, src1_d, ids_d, dst_d, ne00, ne01, s01, s11, s1,
+            mul_mat_f_switch_cols_per_block<half, float>(src0_d, src1_d, ids_d, dst_d, ne00, ne01, ncols_dst, s01, s11, s1,
                 ne02, nchannels_y, nchannels_dst, s02, stride_channel_y, stride_channel_dst,
                 ne03,              ne3,           s03, s13,              s3,                 ctx.stream());
         } break;
