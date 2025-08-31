6c5c6a919899ab1b1f0b8f5f2878cc71b42ddf92
Johannes Gäßler <johannesg@5d6.de>
2025-08-31 18:44:44 +0200

    WIP

--- ggml/src/ggml-cuda/fattn-tile-f32.cu
+++ ggml/src/ggml-cuda/fattn-tile-f32.cu
@@ -3,11 +3,11 @@
 #include "fattn-tile-f32.cuh"
 
 static int fattn_tile_get_kq_stride_host(const int D, const int ncols, const int /*cc*/) {
-    return 32;
+    return D <= 128 || ncols == 16 ? 64 : 32;
 }
 
 static constexpr __device__ int fattn_tile_get_kq_stride_device(int D, int ncols) {
-    return 32;
+    return D <= 128 || ncols == 16 ? 64 : 32;
 }
 
 template<int D, int ncols, int nwarps, bool use_logit_softcap> // D == head size
