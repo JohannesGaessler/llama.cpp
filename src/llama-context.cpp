1531e5dd31397640a7914fcaae587e062ced409a
Johannes Gäßler <johannesg@5d6.de>
2025-09-06 15:05:39 +0200

    WIP

--- src/llama-context.cpp
+++ src/llama-context.cpp
@@ -2819,8 +2819,8 @@ llama_backend_info_data llama_backend_info(const struct llama_context * ctx, siz
 
 void llama_backend_print_memory(const struct llama_context * ctx) {
     const size_t backend_count = llama_backend_count(ctx);
-    std::vector<std::array<std::string, 8>> table_data(backend_count + 1);
-    table_data[0] = {"", "", "total", "free", "model", "context", "compute", "other"};
+    std::vector<std::array<std::string, 9>> table_data(backend_count + 1);
+    table_data[0] = {"", "", "total", "free", "self", "model", "context", "compute", "other"};
 
     constexpr size_t MiB = 1024 * 1024;
     const std::vector<std::string> desc_prefixes_strip = {"NVIDIA ", "GeForce ", "Tesla ", "AMD ", "Radeon ", "Instinct "};
@@ -2846,10 +2846,11 @@ void llama_backend_print_memory(const struct llama_context * ctx) {
         table_data[i + 1][1] = "(" + desc + "):";
         table_data[i + 1][2] = std::to_string(info.device.memory_total / MiB);
         table_data[i + 1][3] = std::to_string(info.device.memory_free / MiB);
-        table_data[i + 1][4] = std::to_string(info.device.memory_used_self_model / MiB);
-        table_data[i + 1][5] = std::to_string(info.device.memory_used_self_context / MiB);
-        table_data[i + 1][6] = std::to_string(info.device.memory_used_self_compute / MiB);
-        table_data[i + 1][7] = std::to_string(info.device.memory_used_other / MiB);
+        table_data[i + 1][4] = std::to_string(info.device.memory_used_self / MiB);
+        table_data[i + 1][5] = std::to_string(info.device.memory_used_self_model / MiB);
+        table_data[i + 1][6] = std::to_string(info.device.memory_used_self_context / MiB);
+        table_data[i + 1][7] = std::to_string(info.device.memory_used_self_compute / MiB);
+        table_data[i + 1][8] = std::to_string(info.device.memory_used_other / MiB);
     }
     for (size_t j = 0; j < table_data[0].size(); j++) {
         size_t max_len = 0;
@@ -2872,9 +2873,9 @@ void llama_backend_print_memory(const struct llama_context * ctx) {
     }
     for (size_t i = 0; i < backend_count + 1; i++) {
         const auto & td = table_data[i];
-        LLAMA_LOG_INFO("%s:%s %s %s = %s + %s + %s + %s + %s%s\n",
-            __func__, td[0].c_str(), td[1].c_str(), td[2].c_str(), td[3].c_str(),
-            td[4].c_str(), td[5].c_str(), td[6].c_str(), td[7].c_str(), i == 0 ? "" : " MiB");
+        LLAMA_LOG_INFO("%s:%s %s %s = %s + (%s = %s + %s + %s) + %s%s\n",
+            __func__, td[0].c_str(), td[1].c_str(), td[2].c_str(), td[3].c_str(), td[4].c_str(),
+            td[5].c_str(), td[6].c_str(), td[7].c_str(), td[8].c_str(), i == 0 ? "" : " MiB");
     }
 }
 
