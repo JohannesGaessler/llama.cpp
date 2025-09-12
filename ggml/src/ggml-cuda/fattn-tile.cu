a5619c51397459c6766c7242cefa9d41ef0d17e0
Johannes Gäßler <johannesg@5d6.de>
2025-09-12 20:44:14 +0200

    WIP

--- ggml/src/ggml-cuda/fattn-tile.cu
+++ ggml/src/ggml-cuda/fattn-tile.cu
@@ -144,7 +144,7 @@ static constexpr __device__ int fattn_tile_get_kq_nbatch_device(int D, int ncols
 
 template<int D, int ncols, bool use_logit_softcap> // D == head size
 #ifdef GGML_USE_HIP
-__launch_bounds__(FATTN_TILE_NTHREADS, 1)
+__launch_bounds__(FATTN_TILE_NTHREADS, 2)
 #else
 __launch_bounds__(FATTN_TILE_NTHREADS, 2)
 #endif // GGML_USE_HIP
