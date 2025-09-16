9dc3f6c6a0e2935646f456a51a1239d34ffc2311
Johannes Gäßler <johannesg@5d6.de>
2025-09-17 00:52:39 +0200

    try fix

--- ggml/src/ggml-cuda/fattn-vec.cuh
+++ ggml/src/ggml-cuda/fattn-vec.cuh
@@ -180,13 +180,17 @@ static __global__ void flash_attn_ext_vec(
 #pragma unroll
         for (int j = 0; j < ncols; ++j) {
             const float2 * Q_f2_j = (const float2 *) (Q + j*nb01);
+// #pragma unroll
+//             for (int i0 = 0; i0 < D/2; i0 += nthreads_KQ*cpy_ne) {
+//                 const int i = i0 + (threadIdx.x % nthreads_KQ)*cpy_ne;
+//                 if (ncols <= 2 || ic0 + j < ne01) {
+//                     ggml_cuda_memcpy_1<cpy_nb>(&Q_f2[j][i0/nthreads_KQ],            &Q_f2_j[i]);
+//                     ggml_cuda_memcpy_1<cpy_nb>(&Q_f2[j][i0/nthreads_KQ + cpy_ne/2], &Q_f2_j[i + cpy_ne/2]);
+//                 }
+//             }
 #pragma unroll
-            for (int i0 = 0; i0 < D/2; i0 += nthreads_KQ*cpy_ne) {
-                const int i = i0 + (threadIdx.x % nthreads_KQ)*cpy_ne;
-                if (ncols <= 2 || ic0 + j < ne01) {
-                    ggml_cuda_memcpy_1<cpy_nb>(&Q_f2[j][i0/nthreads_KQ],            &Q_f2_j[i]);
-                    ggml_cuda_memcpy_1<cpy_nb>(&Q_f2[j][i0/nthreads_KQ + cpy_ne/2], &Q_f2_j[i + cpy_ne/2]);
-                }
+            for (int i0 = 0; i0 < D/2; i0 += WARP_SIZE) {
+                Q_f2[j][i0/WARP_SIZE] = Q_f2_j[i0 + threadIdx.x];
             }
 #pragma unroll
             for (int k = 0; k < (D/2)/nthreads_KQ; ++k) {
