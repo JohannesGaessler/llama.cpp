2145a6063f873deeb3bff0a36e3e51ac66c5c402
Johannes Gäßler <johannesg@5d6.de>
2026-02-01 15:52:04 +0100

    try fix

--- ggml/src/ggml-backend-meta.cpp
+++ ggml/src/ggml-backend-meta.cpp
@@ -758,6 +758,8 @@ static enum ggml_status ggml_backend_meta_graph_compute(ggml_backend_t backend, 
                     auto & bcj_other = backend_ctx->backend_configs[j_other];
                     ggml_tensor * node_other = bcj_other.cgraphs[i-1].cgraph.nodes[bcj_other.cgraphs[i-1].cgraph.n_nodes-1];
                     ggml_backend_tensor_copy_async(bcj_other.backend, bcj.backend, node_other, bcj.cgraphs[i].tmp);
+                    ggml_backend_synchronize(bcj.backend);
+                    ggml_backend_synchronize(bcj_other.backend);
                     bcj.cgraphs[i].cgraph.nodes[0] = ggml_add_inplace(bcj.ctx, node, bcj.cgraphs[i].tmp);
                     bcj.cgraphs[i].cgraph.nodes[0]->buffer = bcj.buf;
                 }
