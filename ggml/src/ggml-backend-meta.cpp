77a901705e7bd7fb44cc48c9449a158f3eb7d296
Johannes Gäßler <johannesg@5d6.de>
2026-02-01 22:41:11 +0100

    try fix

--- ggml/src/ggml-backend-meta.cpp
+++ ggml/src/ggml-backend-meta.cpp
@@ -793,11 +793,14 @@ static enum ggml_status ggml_backend_meta_graph_compute(ggml_backend_t backend, 
                     bcj.cgraphs[i].cgraphs_aux.push_back(*cgraph);
                     bcj.cgraphs[i].cgraphs_aux.back().nodes = &bcj.cgraphs[i].nodes_aux.back();
                     bcj.cgraphs[i].cgraphs_aux.back().n_nodes = 1;
-
-                    const ggml_status status = ggml_backend_graph_compute_async(bcj.backend, &bcj.cgraphs[i].cgraphs_aux.back());
-                    if (status != GGML_STATUS_SUCCESS) {
-                        return status;
-                    }
+                }
+                ggml_backend_synchronize(bcj.backend);
+            }
+            for (size_t j = 0; j < n_backends; j++) {
+                auto & bcj = backend_ctx->backend_configs[j];
+                const ggml_status status = ggml_backend_graph_compute_async(bcj.backend, &bcj.cgraphs[i].cgraphs_aux.back());
+                if (status != GGML_STATUS_SUCCESS) {
+                    return status;
                 }
                 ggml_backend_synchronize(bcj.backend);
             }
