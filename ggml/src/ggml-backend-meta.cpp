887ac927071bbcf67fdd1dd98d1f12f2182d2a3e
Johannes Gäßler <johannesg@5d6.de>
2026-03-04 19:45:57 +0100

    WIP

--- ggml/src/ggml-backend-meta.cpp
+++ ggml/src/ggml-backend-meta.cpp
@@ -1058,10 +1058,11 @@ static enum ggml_status ggml_backend_meta_graph_compute(ggml_backend_t backend, 
         for (size_t j_dst = 0; j_dst < n_backends; j_dst++) {
             auto & bcj_dst = backend_ctx->backend_configs[j_dst];
 
-            for (size_t j_src = 0; j_src < n_backends; j_src++) {
+            for (size_t j_src_0 = 0; j_src_0 < n_backends; j_src_0++) {
+                const size_t j_src = j_src_0 == 0 ? j_dst : (j_src_0 - (j_src_0 < j_dst ? 1 : 0));
                 auto & bcj_src = backend_ctx->backend_configs[j_src];
 
-                ggml_backend_tensor_copy_async(bcj_dst.backend, bcj_src.backend, dst_views[j_dst*n_backends], src_views[j_src*n_backends + j_dst]);
+                ggml_backend_tensor_copy_async(bcj_dst.backend, bcj_src.backend, dst_views[j_dst*n_backends], src_views[j_dst*n_backends + j_src]);
             }
         }
         return GGML_STATUS_SUCCESS;
