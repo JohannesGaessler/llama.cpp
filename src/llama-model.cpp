75aba15feac7a45b8027e093e12b3d7392fbede2
Johannes Gäßler <johannesg@5d6.de>
2026-04-04 13:53:28 +0200

    WIP

--- src/llama-model.cpp
+++ src/llama-model.cpp
@@ -242,10 +242,13 @@ struct ggml_backend_meta_split_state llama_meta_device_get_split_state(const str
                 return {std::lcm(blck_size, n_embd_r/n_heads)};
             }
 
+            const int64_t granularity_qkv = std::lcm(blck_size, head_dim);
             if (std::regex_match(tensor_name, pattern_qkv_weight) || std::regex_match(tensor_name, pattern_attn_gate_weight) ||
                     std::regex_match(tensor_name, pattern_ssm_conv1d)) {
-                const int64_t granularity_qkv = std::lcm(blck_size, head_dim);
                 return std::vector<int64_t>(segments.size(), granularity_qkv);
+            }
+            if (std::regex_match(tensor_name, pattern_ssm_dt)) {
+                return std::vector<int64_t>(segments.size(), granularity_qkv / head_dim);
             }
         } else {
             // regular attention
